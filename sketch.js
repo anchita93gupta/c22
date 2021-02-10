9ded7671a2609609064e829e13fd918ac626bf2b
anchita93gupta <76733471+anchita93gupta@users.noreply.github.com>
2021-02-10 16:53:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,39 @@
-const Engine=Matter.Engine;
-const World=Matter.World;
-const Bodies=Matter.Bodies;
+ 
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-var engine,world,object;
+var engine, world;
+var ground, ball;
 
 function setup(){
-  var canvas=createCanvas(400,400);
-  engine=Engine.create();
-  world=engine.world;
-  var object_options={
-    isStatic:true
-  
-  }
-  object=Bodies.rectangle(200,100,50,50,object_options);
-  World.add(world,object);
-  console.log(object);
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
 
+    var ground_options ={
+        isStatic: true
+    }
+
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.rectangle(200,100,20,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
 }
-function draw()
-{
-  background(0);
-  Engine.update(engine);
-  rectMode(CENTER);
-  rect(object.position.x,object.position.y,50,50);
-    
-  
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+
+   
+    rect(ball.position.x, ball.position.y, 20, 20);
 }
